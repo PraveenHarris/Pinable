2ebab92bfb32c87d06ac410d805d1740153fcfab
Praveen David Harris <praveenharris@yahoo.com>
2019-09-06 01:30:52 -0400

    always on top now working

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 const path = require('path')
-const { app, BrowserWindow } = require('electron')
-
+const { app, BrowserWindow, ipcMain } = require('electron')
 
 require('electron-reload')(__dirname, {
     electron: path.join(__dirname, 'node_modules/.bin/electron.cmd')
 })
 
+var win
+
 function createWindow() {
-    var win = new BrowserWindow({
+    win = new BrowserWindow({
         width: 500,
         height: 600,
+        // frame: false,
         webPreferences: {
             nodeIntegration: true
         }
     })
-
-    win.setMenu(null)
-
+    
     win.loadFile('index.html')
 }
+
+ipcMain.on('always-on-top-message', (e, a) => {
+    win.setAlwaysOnTop(a)
+})
 
 app.on('ready', createWindow)
 
