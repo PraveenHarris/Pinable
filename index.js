53f35c0ad7dc0990765c30085f4def077bde0fdf
Praveen David Harris <praveenharris@yahoo.com>
2019-09-06 02:06:27 -0400

    Update formating

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const path = require('path')
-const { app, BrowserWindow, ipcMain } = require('electron')
+const path = require("path");
+const { app, BrowserWindow, ipcMain } = require("electron");
 
-require('electron-reload')(__dirname, {
-    electron: path.join(__dirname, 'node_modules/.bin/electron.cmd')
-})
+require("electron-reload")(__dirname, {
+  electron: path.join(__dirname, "node_modules/.bin/electron.cmd")
+});
 
-var win
+var win;
 
 function createWindow() {
-    win = new BrowserWindow({
-        width: 500,
-        height: 600,
-        // frame: false,
-        webPreferences: {
-            nodeIntegration: true
-        }
-    })
-    
-    win.loadFile('index.html')
+  win = new BrowserWindow({
+    width: 500,
+    height: 600,
+    // frame: false,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadFile("index.html");
 }
 
-ipcMain.on('always-on-top-message', (e, a) => {
-    win.setAlwaysOnTop(a)
-})
+ipcMain.on("always-on-top-message", (e, a) => {
+  win.setAlwaysOnTop(a);
+});
 
-app.on('ready', createWindow)
-
+app.on("ready", createWindow);
